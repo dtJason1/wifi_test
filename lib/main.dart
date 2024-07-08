a33def2faa3e63b9e2e3b979171e8ae0c5379a1f
jason <originalkimchi100@gmail.com>
2024-07-08 11:33:46 +0900

    wifi test

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Future<List<dynamic>> scanWifi() async {
+  Future<List<Widget>> scanWifi() async {
     List<StatelessWidget> finalList = [];
 
     try {
