6c6d0c83a0531ab89938ae53e459ea754ebb09e7
jason <originalkimchi100@gmail.com>
2024-07-09 10:38:53 +0900

    keyboard update

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 
 import 'package:flutter/rendering.dart';
-import 'package:provider/provider.dart';
 import 'package:wifi_test/provider.dart';
 
 void main() {
@@ -15,29 +14,10 @@ void main() {
   //   child: MyApp(),
   //
   // );
-  MyApp();
+  fakepage();
+
 }
 
-
-// raspberry nmcli 설치됨
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: fakepage()
-    );
-    // const MyHomePage(title: 'Flutter Demo Home Page'),
-  }
-}
 
 class fakepage extends StatelessWidget{
   @override
@@ -46,223 +26,251 @@ class fakepage extends StatelessWidget{
     return Container();
   }
 
-}
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-// sdf
-  void popup(){
-    showDialog(context: context,
-        builder: (context){
-          return Dialog(
-
-            child:
-              Consumer<WifiProvider>(
-                builder: (context, provider,builder) {
-                  if (provider.wifiList == []) {
-                    return SizedBox(
-                        width: 300,
-                        height: 300,
-                        child: CircularProgressIndicator());
-                  }
-                  else {
-                    return Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: SizedBox(
-                        height: 400,
-                        child: Column(
-
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  vertical: 25.0),
-                              child: Text("WIFI Lists",
-                                style: TextStyle(fontWeight: FontWeight.bold),),
-                            ),
-                            SizedBox(
-                              height: 300,
-                              width: 300,
-                              child: ListView(
-                                shrinkWrap: true,
-                                children: provider.wifiList,
-
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  }
-                }
-              ),
-          );
-        }
-    );
-
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-
-      body: Container(
-        width: 1200,
-        height: 800,
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(onPressed: popup, child: Text("hi")), // 누를때 scanwifi 한번,
-
-          ],
-        ),
-      ),
- );
-  }
-}
 
 //
-// class MyButton extends StatefulWidget{
-//   MyButton({required this.text, required this.iscurrentuse});
-//   final String text;
-//   final bool iscurrentuse;
+// // raspberry nmcli 설치됨
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 //
+//   // This widget is the root of your application.
 //   @override
-//   State<MyButton> createState() => _MyButtonState();
-// }
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
 //
-// class _MyButtonState extends State<MyButton> {
-//   String myPW = "";
-//   late TextEditingController _controllerText;
-//   bool shiftEnabled = false;
-//   bool isNumericMode = false;
-//   bool _show = false;
-//   @override
-//   void initState() {
-//     _controllerText = TextEditingController();
-//     super.initState();
-//   }
-//   @override
-//   Widget build(BuildContext context){
-//     return TextButton(onPressed: (){
-//       showDialog(context: context, builder:(context) {
-//         return Dialog(
-//           child: SizedBox(
-//             width: 300,
-//             height: 300,
-//             child: Column(
-//               children: [
-//               Row(
-//                 children: [
-//                   Padding(
-//                     padding: const EdgeInsets.all(8.0),
-//                     child: Text("SSID"),
-//                   ),
-//                   Padding(
-//                     padding: const EdgeInsets.all(8.0),
-//                     child: Text(widget.text),
-//                   ),
-//
-//
-//                 ],
-//               ),
-//
-//               Row(
-//                   children: [
-//                     Padding(
-//                       padding: const EdgeInsets.all(8.0),
-//                       child: Text("Password"),
-//                     ),
-//                     Padding(
-//                       padding: const EdgeInsets.all(8.0),
-//                       child: GestureDetector(
-//                         onTap: (){setState(() {
-//                           _show = true;
-//                         });},
-//                         child: Container(
-//                           width: 200,
-//                           height: 40,
-//                         ),
-//                       )
-//                     )
-//                   ],
-//                 ),
-//
-//
-//                 TextButton(onPressed: () async{
-//                   try {
-//                     print("${widget.text }, , ${myPW}");
-//                     await Process.run('nmcli',['device', 'wifi', 'connect', '${widget.text}', 'password', '$myPW']).then((value) => Navigator.of(context).pop());
-//
-//
-//                   } on Exception catch (e) {
-//                     print(e);
-//                     // TODO
-//                   }
-//                 }, child: Text("confirm")),
-//                 AnimatedContainer(duration: Duration(seconds: 1),
-//                   color: Colors.deepPurple,
-//                   child: VirtualKeyboard(
-//                       height: _show ? 300 : 0,
-//                       //width: 500,
-//                       textColor: Colors.white,
-//                       textController: _controllerText,
-//                       //customLayoutKeys: _customLayoutKeys,
-//                       defaultLayouts: [
-//                         VirtualKeyboardDefaultLayouts.English
-//                       ],
-//                       //reverseLayout :true,
-//                       type: isNumericMode
-//                           ? VirtualKeyboardType.Numeric
-//                           : VirtualKeyboardType.Alphanumeric,
-//                       onKeyPress: _onKeyPress),
-//                 )
-//
-//               ],),
-//           ),
-//         );
-//
-//       }
-//       );
-//
-//     },
-//       child: Text(widget.text, style: TextStyle(color: this.widget.iscurrentuse ? Colors.blue : Colors.black),),
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
 //     );
 //   }
-//   _onKeyPress(VirtualKeyboardKey key) {
-//     if (key.keyType == VirtualKeyboardKeyType.String) {
-//       myPW = myPW + ((shiftEnabled ? key.capsText : key.text) ?? '');
-//     } else if (key.keyType == VirtualKeyboardKeyType.Action) {
-//       switch (key.action) {
-//         case VirtualKeyboardKeyAction.Backspace:
-//           if (myPW.length == 0) return;
-//           myPW = myPW.substring(0, myPW.length - 1);
-//           break;
-//         case VirtualKeyboardKeyAction.Return:
-//           myPW = myPW + '\n';
-//           break;
-//         case VirtualKeyboardKeyAction.Space:
-//           myPW = myPW + (key.text ?? '');
-//           break;
-//         case VirtualKeyboardKeyAction.Shift:
-//           shiftEnabled = !shiftEnabled;
-//           break;
-//         default:
-//       }
-//     }
-//     // Update the screen
-//     setState(() {
-//       myPW = myPW;
+// }
 //
-//     });
+// class fakepage extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext context){
+//
+//     return Container()
+//   }
+//
+// }
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+//
+//
+//   final String title;
+//
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+// // sdf
+//   void popup(){
+//     showDialog(context: context,
+//         builder: (context){
+//           return Dialog(
+//
+//             child:
+//               Consumer<WifiProvider>(
+//                 builder: (context, provider,builder) {
+//                   if (provider.wifiList == []) {
+//                     return SizedBox(
+//                         width: 300,
+//                         height: 300,
+//                         child: CircularProgressIndicator());
+//                   }
+//                   else {
+//                     return Padding(
+//                       padding: const EdgeInsets.all(10.0),
+//                       child: SizedBox(
+//                         height: 400,
+//                         child: Column(
+//
+//                           children: [
+//                             Padding(
+//                               padding: const EdgeInsets.symmetric(
+//                                   vertical: 25.0),
+//                               child: Text("WIFI Lists",
+//                                 style: TextStyle(fontWeight: FontWeight.bold),),
+//                             ),
+//                             SizedBox(
+//                               height: 300,
+//                               width: 300,
+//                               child: ListView(
+//                                 shrinkWrap: true,
+//                                 children: provider.wifiList,
+//
+//                               ),
+//                             ),
+//                           ],
+//                         ),
+//                       ),
+//                     );
+//                   }
+//                 }
+//               ),
+//           );
+//         }
+//     );
+//
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//
+//     return Scaffold(
+//
+//       body: Container(
+//         width: 1200,
+//         height: 800,
+//         child: Column(
+//
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             ElevatedButton(onPressed: popup, child: Text("hi")), // 누를때 scanwifi 한번,
+//
+//           ],
+//         ),
+//       ),
+//  );
 //   }
 // }
+//
+// //
+// // class MyButton extends StatefulWidget{
+// //   MyButton({required this.text, required this.iscurrentuse});
+// //   final String text;
+// //   final bool iscurrentuse;
+// //
+// //   @override
+// //   State<MyButton> createState() => _MyButtonState();
+// // }
+// //
+// // class _MyButtonState extends State<MyButton> {
+// //   String myPW = "";
+// //   late TextEditingController _controllerText;
+// //   bool shiftEnabled = false;
+// //   bool isNumericMode = false;
+// //   bool _show = false;
+// //   @override
+// //   void initState() {
+// //     _controllerText = TextEditingController();
+// //     super.initState();
+// //   }
+// //   @override
+// //   Widget build(BuildContext context){
+// //     return TextButton(onPressed: (){
+// //       showDialog(context: context, builder:(context) {
+// //         return Dialog(
+// //           child: SizedBox(
+// //             width: 300,
+// //             height: 300,
+// //             child: Column(
+// //               children: [
+// //               Row(
+// //                 children: [
+// //                   Padding(
+// //                     padding: const EdgeInsets.all(8.0),
+// //                     child: Text("SSID"),
+// //                   ),
+// //                   Padding(
+// //                     padding: const EdgeInsets.all(8.0),
+// //                     child: Text(widget.text),
+// //                   ),
+// //
+// //
+// //                 ],
+// //               ),
+// //
+// //               Row(
+// //                   children: [
+// //                     Padding(
+// //                       padding: const EdgeInsets.all(8.0),
+// //                       child: Text("Password"),
+// //                     ),
+// //                     Padding(
+// //                       padding: const EdgeInsets.all(8.0),
+// //                       child: GestureDetector(
+// //                         onTap: (){setState(() {
+// //                           _show = true;
+// //                         });},
+// //                         child: Container(
+// //                           width: 200,
+// //                           height: 40,
+// //                         ),
+// //                       )
+// //                     )
+// //                   ],
+// //                 ),
+// //
+// //
+// //                 TextButton(onPressed: () async{
+// //                   try {
+// //                     print("${widget.text }, , ${myPW}");
+// //                     await Process.run('nmcli',['device', 'wifi', 'connect', '${widget.text}', 'password', '$myPW']).then((value) => Navigator.of(context).pop());
+// //
+// //
+// //                   } on Exception catch (e) {
+// //                     print(e);
+// //                     // TODO
+// //                   }
+// //                 }, child: Text("confirm")),
+// //                 AnimatedContainer(duration: Duration(seconds: 1),
+// //                   color: Colors.deepPurple,
+// //                   child: VirtualKeyboard(
+// //                       height: _show ? 300 : 0,
+// //                       //width: 500,
+// //                       textColor: Colors.white,
+// //                       textController: _controllerText,
+// //                       //customLayoutKeys: _customLayoutKeys,
+// //                       defaultLayouts: [
+// //                         VirtualKeyboardDefaultLayouts.English
+// //                       ],
+// //                       //reverseLayout :true,
+// //                       type: isNumericMode
+// //                           ? VirtualKeyboardType.Numeric
+// //                           : VirtualKeyboardType.Alphanumeric,
+// //                       onKeyPress: _onKeyPress),
+// //                 )
+// //
+// //               ],),
+// //           ),
+// //         );
+// //
+// //       }
+// //       );
+// //
+// //     },
+// //       child: Text(widget.text, style: TextStyle(color: this.widget.iscurrentuse ? Colors.blue : Colors.black),),
+// //     );
+// //   }
+// //   _onKeyPress(VirtualKeyboardKey key) {
+// //     if (key.keyType == VirtualKeyboardKeyType.String) {
+// //       myPW = myPW + ((shiftEnabled ? key.capsText : key.text) ?? '');
+// //     } else if (key.keyType == VirtualKeyboardKeyType.Action) {
+// //       switch (key.action) {
+// //         case VirtualKeyboardKeyAction.Backspace:
+// //           if (myPW.length == 0) return;
+// //           myPW = myPW.substring(0, myPW.length - 1);
+// //           break;
+// //         case VirtualKeyboardKeyAction.Return:
+// //           myPW = myPW + '\n';
+// //           break;
+// //         case VirtualKeyboardKeyAction.Space:
+// //           myPW = myPW + (key.text ?? '');
+// //           break;
+// //         case VirtualKeyboardKeyAction.Shift:
+// //           shiftEnabled = !shiftEnabled;
+// //           break;
+// //         default:
+// //       }
+// //     }
+// //     // Update the screen
+// //     setState(() {
+// //       myPW = myPW;
+// //
+// //     });
+// //   }
+// // }
