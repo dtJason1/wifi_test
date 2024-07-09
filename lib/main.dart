8e9aa1b75d231a12d2ea3a9ccfa477164a5ab4a8
jason <originalkimchi100@gmail.com>
2024-07-09 12:39:21 +0900

    ss

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,38 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             ),
           ),
-          isSelected ? Container(color: Colors.red,) : Container()
+          isSelected ? Consumer<WifiProvider>(
+            builder: (context, provider, child) {
+              return Container(
+                child: Container(child:Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: SizedBox(
+                    height: 400,
+                    child: Column(
+
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 25.0),
+                          child: Text("WIFI Lists",style: TextStyle(fontWeight: FontWeight.bold),),
+                        ),
+
+                        SizedBox(
+                          height: 300,
+                          width: 300,
+                          child: ListView(
+                            shrinkWrap: true,
+                            children: provider.wifiList,
+
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+                  ,),
+              );
+            }
+          ) : Container()
         ],
       ),
  );
