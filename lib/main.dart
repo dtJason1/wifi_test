7b0828e44147f857527c03a917e1f9a3a8e3de79
jason <originalkimchi100@gmail.com>
2024-07-09 12:26:53 +0900

    ss

--- lib/main.dart
+++ lib/main.dart
@@ -50,31 +50,35 @@ class _MyHomePageState extends State<MyHomePage> {
           return Dialog(
 
             child:
-              Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: SizedBox(
-                height: 400,
-                child: Column(
+              Consumer<WifiProvider>(
+                builder: (context, provider, child) {
+                  return Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: SizedBox(
+                    height: 400,
+                    child: Column(
 
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 25.0),
-                      child: Text("WIFI Lists",style: TextStyle(fontWeight: FontWeight.bold),),
+                      children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 25.0),
+                            child: Text("WIFI Lists",style: TextStyle(fontWeight: FontWeight.bold),),
+                          ),
+
+                        SizedBox(
+                          height: 300,
+                          width: 300,
+                          child: ListView(
+                            shrinkWrap: true,
+                            children: provider.wifiList,
+
+                          ),
+                        ),
+                      ],
                     ),
-
-                    SizedBox(
-                      height: 300,
-                      width: 300,
-                      child: ListView(
-                        shrinkWrap: true,
-                        children: [],
-
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            )
+                  ),
+                              );
+                }
+              )
           );
         }
     );
