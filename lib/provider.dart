1ef4f89ff36f9cc3b7024ee7723b3a63bc34b68c
jason <originalkimchi100@gmail.com>
2024-07-12 16:37:10 +0900

    update wifi

--- lib/provider.dart
+++ lib/provider.dart
@@ -299,7 +299,7 @@ class _Dialog2State extends State<Dialog2> {
 
                         Process.run('nmcli',['dev', 'wifi', 'connect', '${widget.text}', 'password', '${controller.text}']).then((value) {
                           print("controller text : ${controller.text}");
-                          print("connected ${value.stdout}");
+                          print("stdout ${value.stdout}");
                           print("err: ${value.stderr}");
                           keyboardkey.clearKey();
                           Future.delayed(const Duration(seconds: 1)).then((value) {
