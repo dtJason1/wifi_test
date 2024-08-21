473677aeadf1eea2ab3ed2f56c204d959c5d46f3
jason <originalkimchi100@gmail.com>
2024-08-21 15:52:02 +0900

    dialog update

--- lib/main.dart
+++ lib/main.dart
@@ -217,14 +217,7 @@ class _MainPageState extends State<MainPage> {
               });
 
             },
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              color: Color.fromRGBO(0, 0, 0, 0.1),
 
-
-
-            ),
 
           ),
         ),
