186bcee593ae81bfe294f3ef53783d51d7911490
jason <originalkimchi100@gmail.com>
2024-08-22 17:27:53 +0900

    header bar update

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:wifi_test/upper_container.dart';
@@ -25,7 +27,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   int _animationTime = 500;
   bool playedOnce = true;
 
-
+  String _timeData = "";
   @override
   void initState(){
     _animationTime = startAnimationTime;
@@ -107,7 +109,15 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     DateTime now = DateTime.now();
-    String formattedDate = DateFormat('kk:mm:ss \n EEE d MMM').format(now);
+    String check_time(BuildContext context){ //context는 Snackbar용, 다른 방식으로 출력할거면 필요없음.
+      var now = new DateTime.now(); //반드시 다른 함수에서 해야함, Mypage같은 클래스에서는 사용 불가능
+      String formatDate = DateFormat('HH:mm').format(now); //format변경
+      return formatDate;
+    }
+    Timer.periodic(Duration(seconds: 2), (timer) {
+      _timeData = check_time(context);
+
+    });
     return Scaffold(
 
       body: Stack(
@@ -128,8 +138,10 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: <Widget>[
                           Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+
                             children: [
-                              Text(formattedDate,textAlign: TextAlign.center,style: new TextStyle(fontWeight: FontWeight.bold,fontSize: 25.0),),
+                              Text(_timeData,textAlign: TextAlign.center,style: new TextStyle(fontWeight: FontWeight.bold,fontSize: 25.0),),
                               IconButton(onPressed: (){
                                 Navigator.push(context, MaterialPageRoute(builder: (context) => MainPage()));
 
