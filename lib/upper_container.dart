ebf6d7a20a17ac181be3544f9b00b87d37b43791
jason <originalkimchi100@gmail.com>
2024-08-22 10:46:40 +0900

    upper bar updated

--- lib/upper_container.dart
+++ lib/upper_container.dart
@@ -9,7 +9,7 @@ class UpperContainer extends StatelessWidget{
   Widget build(BuildContext context){
     return FadeInDemo(
       controller: animationController,
-      child: Container(color: Colors.black, height: 50, width: 1200,  alignment: Alignment.center,child: Text("Hello", style: TextStyle(color: Colors.white , fontSize: 16),),),
+      child: Container(color: Colors.black, height: 50, width: 1200,  alignment: Alignment.center,child: Text("${MediaQuery.of(context).size.width}", style: TextStyle(color: Colors.white , fontSize: 16),),),
     );
 
 
